98377f181c98ab9aada18bec2526a0746346b824
274322055@qq.com <274322055@qq.com>
2021-12-16 10:57:23 +0800

    init

---
+++ src/main/java/org/bill/mock/Test.java
@@ -0,0 +1,11 @@
+package org.bill.mock;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map aa = new HashMap();
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/org/bill/mock/process/ProcessTest.java
@@ -0,0 +1,20 @@
+package org.bill.mock.process;
+
+public class ProcessTest {
+
+    public static void main(String[] args) {
+//        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+//                .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
+//                .setJdbcUsername("sa")
+//                .setJdbcPassword("")
+//                .setJdbcDriver("org.h2.Driver");
+////                .setDatabaseSchemaUpdate(ProcessEngineConfiguration.);
+//
+//        ProcessEngine processEngine = cfg.buildProcessEngine();
+
+    }
+
+
+}
+
+
